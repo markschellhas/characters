1df5b9d56814b8fc4195cc7046ed05506c539ef1
Mark Schellhas <markschellhas@Marks-MacBook-Pro.local>
2024-11-28 05:22:16 +1100

    adds root aggregator import too

---
+++ index.js
@@ -0,0 +1,57 @@
+// Import all sub-packages dynamically
+import a from '@characters/a';
+import b from '@characters/b';
+import c from '@characters/c';
+import d from '@characters/d';
+import e from '@characters/e';
+import f from '@characters/f';
+import g from '@characters/g';
+import h from '@characters/h';
+import i from '@characters/i';
+import j from '@characters/j';
+import k from '@characters/k';
+import l from '@characters/l';
+import m from '@characters/m';
+import n from '@characters/n';
+import o from '@characters/o';
+import p from '@characters/p';
+import q from '@characters/q';
+import r from '@characters/r';
+import s from '@characters/s';
+import t from '@characters/t';
+import u from '@characters/u';
+import v from '@characters/v';
+import w from '@characters/w';
+import x from '@characters/x';
+import y from '@characters/y';
+import z from '@characters/z';
+
+// Export all characters as a default object
+export default {
+  a,
+  b,
+  c,
+  d,
+  e,
+  f,
+  g,
+  h,
+  i,
+  j,
+  k,
+  l,
+  m,
+  n,
+  o,
+  p,
+  q,
+  r,
+  s,
+  t,
+  u,
+  v,
+  w,
+  x,
+  y,
+  z
+};
